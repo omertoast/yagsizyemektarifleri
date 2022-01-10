fe6d3376bf43ed42f04e428b391683a317f8d050
Ömer Talha Özdemir <omertoast@gmail.com>
2022-01-10 14:41:12 +0300

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,15 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Yağsız Yemek Tarifleri</title>
         <link rel="shortcut icon" href="/yagsizlogo.png" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFV615XQN8"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZFV615XQN8');
+        </script>
       </Head>
 
       <Header />
