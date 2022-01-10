2f67202878ef1b7a3f2fb2f0a8d02bd7b7baf0ac
Ömer Talha Özdemir <omertoast@gmail.com>
2022-01-10 15:01:13 +0300

    google analytics fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,52 @@
 import 'tailwindcss/tailwind.css'
 
+import Script from 'next/script'
 import Head from 'next/head'
 import Header from '../components/header'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Auth0Provider } from '@auth0/auth0-react'
+import * as gtag from '../lib/gtag'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Auth0Provider
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
     >
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Yağsız Yemek Tarifleri</title>
-        <link rel="shortcut icon" href="/yagsizlogo.png" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFV615XQN8"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-ZFV615XQN8');
-        </script>
       </Head>
 
       <Header />
